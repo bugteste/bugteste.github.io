dbf8ff0c9e5b5d0778182eb9b628f300b759eba4
bugteste <buggy.x@outlook.com.br>
2018-03-08 04:07:18 -0300

    js

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,11 +1,12 @@
-$(document).read(function(){
-
+  $(document).ready(function () {
 
   $("#listar").click(function(){
     console.log("listando!");
+    return false;
   };
 
-  $("#sincronizar").click(function(){
+  $(".sincronizar").click(function(){
     console.log("sincronizando!");
+    return false;
   };
 }
